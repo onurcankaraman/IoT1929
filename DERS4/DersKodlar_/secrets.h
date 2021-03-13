771faccf730070c8529b9dbfbd2b715df8526e91
ONUR CAN KARAMAN <47088037+onurcankaraman@users.noreply.github.com>
2021-03-13 18:39:08 +0300

    Ders5

---
+++ DERS4/DersKodlar_/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Onurcan"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "vtn102asd"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1326758			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "NNSP2E5CSJGNK9HO"   // replace XYZ with your channel write API Key
