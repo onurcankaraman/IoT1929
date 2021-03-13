2fcd204deeda4510545ba2743b98eb821602ccd2
ONUR CAN KARAMAN <47088037+onurcankaraman@users.noreply.github.com>
2021-03-13 20:52:35 +0300

    ı2c Örneği

---
+++ DERS5/Gyroscope/Gyroscope.ino
@@ -0,0 +1,36 @@
+#include<Wire.h>
+const int MPU=0x68; // Sensörümün Adresi
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; // Değişken
+
+void setup(){
+  Wire.begin(); // İletişim Başlat
+  Wire.beginTransmission(MPU); //Cihaz Şeç
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+void loop(){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+  
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ); 
+  
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(GyX);
+  Serial.print(" | Y = "); Serial.print(GyY);
+  Serial.print(" | Z = "); Serial.println(GyZ);
+  Serial.println(" ");
+  delay(333);
+}
